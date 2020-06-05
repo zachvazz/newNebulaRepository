8cb8e6a4b87c854b0edcba7c4dfd76f3aa9fba00
RTennis2004 <49493850+RTennis2004@users.noreply.github.com>
2020-06-04 20:21:13 -0400

    adding dependencies

--- screens/Search.js
+++ screens/Search.js
@@ -1,48 +1,19 @@
-import React, { Component } from 'react';
-import {StyleSheet, View, Image, Text } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import SearchBar from "react-native-dynamic-search-bar"
 
-export default class Search extends Component {
-    render() {
-        return (
-                <View style={StyleSheet.container}>
-                    <View style={styles.logoContainer}>
-                        <Image 
-                        style={styles.logo}
-                        source={require('../../assets/Physicus.png')}
-                        />
-                        <Text style={styles.title}>An app made for online physiotherapy</Text>
-                    </View>
-                    <View style={styles.logoContainer}>
-                    <LoginForm />
-                    </View>
-                </View>
-       );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFF8DC',
-        justifyContent: 'center',
-        alignItems: 'center'    
-    },
-    logoContainer: {
-        marginTop: 100,
-        alignItems:'center',
-        flexGrow: 1,
-        justifyContent : 'center'
-    },
-    logo: {
-        width: 100,
-        height: 100
-    },
-    title: {
-        color: '#000000',
-        marginTop: 10,
-        width: 160,
-        textAlign: 'center',
-        opacity: 0.9
-    }
-});
+<SearchBar
+   onPressToFocus
+   autoFocus={false}
+   fontColor="#c6c6c6"
+   iconColor="#c6c6c6"
+   shadowColor="#282828"
+   cancelIconColor="#c6c6c6"
+   backgroundColor="#353d5e"
+   placeholder="Search here"
+   onChangeText={text => {
+     this.filterList(text);
+   }}
+   onPressCancel={() => {
+     this.filterList("");
+   }}
+   onPress={() => alert("onPress")}
+ />
